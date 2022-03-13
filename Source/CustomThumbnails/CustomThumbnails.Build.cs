97aa5cc1425036a89eb5585b8f296288fe73e241
VernamRD <vernamrdcross@mail.ru>
2022-03-13 20:34:08 +0300

    Update CustomThumbnails.Build.cs

--- Source/CustomThumbnails/CustomThumbnails.Build.cs
+++ Source/CustomThumbnails/CustomThumbnails.Build.cs
@@ -6,7 +6,7 @@ public class CustomThumbnails : ModuleRules
 {
 	public CustomThumbnails(ReadOnlyTargetRules Target) : base(Target)
 	{
-		
+		bUsePrecompiled = true;
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
 		
 		PublicIncludePaths.AddRange(
